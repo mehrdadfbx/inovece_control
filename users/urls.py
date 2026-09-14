7379ca0fcc9ba9dd5704d6cfe8247a078c716238
moshref <103120775+mehrdadfbx@users.noreply.github.com>
2026-09-14 12:32:16 +0330

    users  view sets completed

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import  path
+from .views import OwnerViewSet, CustomerViewSet
+
+
+urlpatterns = [
+    path('owners/', OwnerViewSet.as_view({'get': 'list', 'post': 'create'}), name='owner-list'),
+    path('owners/<int:pk>/', OwnerViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='owner-detail'),
+    path('customers/', CustomerViewSet.as_view({'get': 'list', 'post': 'create'}), name='customer-list'),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .Serializer import OwnerSerializer, CustomerSerializer
+from users.models import Owner, Customer
+
+
+class OwnerViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer

d8031713a542893761bde948978957559de72477
moshref <103120775+mehrdadfbx@users.noreply.github.com>
2026-09-19 12:30:32 +0330

    admin code add
    add admin for super user and admin

--- Invoics/admin.py
+++ Invoics/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Inventory, Invoice, InventoryLog, InvoiceItem
 # Register your models here.
+class InvoceAdmin(admin.ModelAdmin):
+    list_display  = ('user', 'status', 'order_date',)
+    list_filter   = ('user', 'status',)
+    search_fields = ('user', 'status',)
+    ordering      = ('user',)
+
+admin.site.register (Invoice, InvoceAdmin)
+
+class InventoryAdmin(admin.ModelAdmin):
+    list_display  = ('product', 'warehouse_location', 'quantity',)
+    list_filter   = ('product', 'warehouse_location', 'quantity',)
+    search_fields = ('product', 'warehouse_location', 'quantity')
+    ordering      = ('warehouse_location',)
+
+admin.site.register(Inventory, InventoryAdmin)
+
+
--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import Category, Company, Product
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('company', 'name', 'is_active',)
+    list_filter  = ('company' ,'name')
+
+
+admin.site.register(Category, CategoryAdmin)
+
+class ComponyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active',)
+    list_filter  = ('name')
+
+admin.site.register(Company, ComponyAdmin)
+
+class ProductAdmin(admin.modelAdmin):
+    list_display = ('company', 'category', 'name', 'price', 'is_active')
+    list_filter  = ('company', 'category', 'price', 'is_active')
+
+admin.site.register(Product, ProductAdmin)
